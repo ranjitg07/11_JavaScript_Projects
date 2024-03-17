3aa8d7c75d34256f81de05678462756fa285fa05
Ranjit Gupta <ranjitgupta2002@gmail.com>
2024-03-17 19:32:02 +0530

    sorted each section for html, styles & scripts + made bmi calculator

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Todo Script
+
 let input = document.getElementById("taskInput");
 let btn_add = document.querySelector(".add");
 let ul = document.getElementById("taskList");
@@ -23,3 +25,51 @@ function addTask() {
     input.value = "";
   }
 }
+
+// BMI - Calculator Script
+
+let bmiBtn = document.querySelector('.btn-calc')
+
+bmiBtn.addEventListener('click', calculateBMI)
+
+function calculateBMI(){
+    let weight = document.getElementById('weight').value;
+    let height = document.getElementById('height').value;
+
+    let bmi = (weight / ((height / 100) * (height / 100))).toFixed(2);
+
+    let bmiResult = "Your BMI is: " + bmi;
+    if(bmi < 18.5) {
+        bmiResult += "(UnderWeight)"
+    } else if(bmi >= 18.5 && bmi < 25){
+        bmiResult += "(NormalWeight)"
+    } else if(bmi >= 25 && bmi < 30){
+        bmiResult += "(OverWeight)"
+    } else if(bmi >= 30 && bmi < 40){
+        bmiResult += "(Obese)"
+    }
+    document.getElementById('bmi-result').innerText = bmiResult;
+}
+
+// Copy Clipboard Script
+
+
+// Quote-Generator Script
+
+
+// Password-Generator Script
+
+
+// Digital-Clock Script
+
+
+// QR-Code Script
+
+
+// Drawing-App Script
+
+
+// Weather-App Script
+
+
+// Recipe-Finder Script
