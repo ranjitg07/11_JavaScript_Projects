c6bb97d3a6886ec657b96fcfd02bbc4de1e2da08
Ranjit Gupta <ranjitgupta2002@gmail.com>
2024-03-29 18:25:48 +0530

    updated styles

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 // Todo Script
 
-let input = document.getElementById("taskInput");
-let btn_add = document.querySelector(".add");
-let ul = document.getElementById("taskList");
 
-btn_add.addEventListener("click", addTask);
 
-function addTask() {
-  let taskText = input.value.trim();
-  if (taskText !== "") {
-    let li = document.createElement("li");
-    li.textContent = taskText;
 
-    let deleteBtn = document.createElement("button");
-    deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
-    deleteBtn.className = "delete";
-    deleteBtn.onclick = function () {
-      li.remove();
-    };
+// let input = document.getElementById("taskInput");
+// let btn_add = document.querySelector(".addTask");
+// let ul = document.getElementById("taskList");
 
-    li.appendChild(deleteBtn);
-    ul.appendChild(li);
+// btn_add.addEventListener("click", addTask);
 
-    input.value = "";
-  }
-}
+// function addTask() {
+//   let taskText = input.value.trim();
+//   if (taskText !== "") {
+//     let li = document.createElement("li");
+//     li.textContent = taskText;
+
+//     let deleteBtn = document.createElement("button");
+//     deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+//     deleteBtn.className = "delete";
+//     deleteBtn.onclick = function () {
+//       li.remove();
+//     };
+
+//     li.appendChild(deleteBtn);
+//     ul.appendChild(li);
+
+//     input.value = "";
+//   }
+// }
 
 // Calculator Script
 
