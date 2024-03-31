c12749c4b9b73baf51ea94240ed8756d92d68000
Ranjit Gupta <ranjitgupta2002@gmail.com>
2024-03-31 21:03:02 +0530

    done todo section

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // Todo Script
 
+document.addEventListener('DOMContentLoaded', () => {
+  const storedTasks = JSON.parse(localStorage.getItem('tasks'))
+
+  if(storedTasks){
+    storedTasks.forEach((task) => tasks.push(task));
+    updateTasksList();
+    updateStats();
+    itemTotals();
+  }
+})
+
 let tasks = [];
 const taskInput = document.getElementById('taskInput');
 const taskBtn = document.querySelector('.addTask');
+const deleteAllBtn = document.getElementById('deleteAll')
 
+const saveTask = () => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
 const addTask = () => {
   const taskText = taskInput.value.trim();
@@ -12,17 +27,26 @@ const addTask = () => {
     tasks.push({ text: taskText, completed: false });
     taskInput.value = '';
     updateTasksList();
+    updateStats();
+    itemTotals();
+    saveTask();
   }
 }
 
 const toggleTaskComplete = (index) => {
   tasks[index].completed = !tasks[index].completed;
   updateTasksList();
+  updateStats();
+  itemTotals();
+  saveTask();
 }
 
 const deleteTask = (index) => {
   tasks.splice(index, 1);
   updateTasksList();
+  updateStats();
+  itemTotals();
+  saveTask();
 }
 
 const editTask = (index) => {
@@ -32,6 +56,38 @@ const editTask = (index) => {
   tasks.splice(index, 1);
   // once the task is updated it will add the task to the list
   updateTasksList();
+  updateStats();
+  itemTotals();
+  saveTask();
+}
+
+// Progress Stats
+const updateStats = () => {
+  const completedTasks = tasks.filter(task => task.completed).length;
+  const totalTasks = tasks.length;
+  const progress = (completedTasks / totalTasks) * 100;
+
+  const progressBar = document.getElementById('progress');
+  progressBar.style.width = `${progress}%`
+
+  if(tasks.length && completedTasks === totalTasks){
+    blastConfetti();
+  }
+
+  document.getElementById('todoNumbers').innerText = `${completedTasks} / ${totalTasks}`;
+}
+
+deleteAllBtn.addEventListener('click', () => {
+  tasks = [];
+  updateTasksList();
+  updateStats();
+  itemTotals();
+  saveTask();
+})
+
+const itemTotals = () => {
+  const totalTasks = tasks.length;
+  document.getElementById('itemsCounter').innerText = `${totalTasks} `;
 }
 
 const updateTasksList = () => {
@@ -92,6 +148,47 @@ taskBtn.addEventListener("click", function (e) {
   addTask();
 });
 
+const blastConfetti = () => {
+  const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+}
 
 // Calculator Script
 
